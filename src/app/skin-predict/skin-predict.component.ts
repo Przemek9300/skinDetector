00631b149f893453f9d9af15796a03e07a0bf038
unknown <przemek930010@gmail.com>
2020-04-13 11:35:37 +0200

    fix tfs

--- src/app/skin-predict/skin-predict.component.ts
+++ src/app/skin-predict/skin-predict.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as tf from '@tensorflow/tfjs';
 @Component({
   selector: 'app-skin-predict',
   templateUrl: './skin-predict.component.html',
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkinPredictComponent implements OnInit {
   public models: ['cancer-skin-2020-02-01', 'cancer-skin-2019-02-01'];
+  public modelTF: any;
   public selectedValue: string;
   constructor() {}
 
-  ngOnInit(): void {}
+  public async ngOnInit(): Promise<void> {
+    await this.loadModel();
+  }
+  public async loadModel() {
+    this.modelTF = await tf.loadLayersModel('/assets/model-tf/model.json');
+  }
 }
