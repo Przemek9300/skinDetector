3b60803098f8ec0376b6a598cc8b49e6226d6325
unknown <przemek930010@gmail.com>
2020-06-08 17:59:30 +0200

    add ghp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCQG-_m8XTwBMtesdnv4XN5aCzk03KHOQ0',
+    authDomain: 'skin-detector-3ea9a.firebaseapp.com',
+    databaseURL: 'https://skin-detector-3ea9a.firebaseio.com',
+    projectId: 'skin-detector-3ea9a',
+    storageBucket: 'skin-detector-3ea9a.appspot.com',
+    messagingSenderId: '609037959342',
+    appId: '1:609037959342:web:2e13f662e60a3c92202d3f',
+    measurementId: 'G-9J3CG43CZ3',
+  },
 };
