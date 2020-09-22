4820ab45b0b67a0ec4a8bfb7cb8a6250c6bdef4a
Przemyslaw Gadula <pgadula@prescientco.com>
2020-09-22 20:30:39 +0200

    handle webcam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { PercentPipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { WebcamModule } from 'ngx-webcam';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartComponent } from './charts-container/chart/chart.component';
+import { ChartsComponent } from './charts-container/charts.component';
 import { LayoutComponent } from './layout/layout.component';
 import { MaterialModule } from './material/material.module';
-import { RouterModule } from '@angular/router';
-import { SkinPredictComponent } from './skin-predict/skin-predict.component';
-import { ResultViewComponent } from './skin-predict/result-view/result-view.component';
-import { ChartsComponent } from './charts-container/charts.component';
-import { SkinInformationComponent } from './skin-information/skin-information.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ChartComponent } from './charts-container/chart/chart.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireStorageModule } from 'angularfire2/storage';
 import { ModelUploadComponent } from './model-upload/model-upload.component';
-import { environment } from 'src/environments/environment';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SkinInformationComponent } from './skin-information/skin-information.component';
+import { ResultViewComponent } from './skin-predict/result-view/result-view.component';
+import { SkinPredictComponent } from './skin-predict/skin-predict.component';
 import { appReducer } from './store/reducer';
-import { PercentPipe } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,6 +47,7 @@ import { PercentPipe } from '@angular/common';
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
+    WebcamModule,
     NgxSpinnerModule,
     StoreModule.forRoot({ appState: appReducer }, {}),
     StoreDevtoolsModule.instrument({
--- src/app/skin-predict/skin-predict.component.ts
+++ src/app/skin-predict/skin-predict.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
-import { HAM10000Service } from '../services/ham10000.service';
-import { Observable } from 'rxjs';
-import { AngularFireStorage } from 'angularfire2/storage';
 import { FormControl, Validators } from '@angular/forms';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { PredictionState } from '../store/reducer';
 import { Store } from '@ngrx/store';
-import { setPredictionResult, setLabel } from '../store/actions';
+import * as tf from '@tensorflow/tfjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
+
+import { HAM10000Service } from '../services/ham10000.service';
+import { setLabel, setPredictionResult } from '../store/actions';
+import { PredictionState } from '../store/reducer';
+
 @Component({
   selector: 'app-skin-predict',
   templateUrl: './skin-predict.component.html',
@@ -28,6 +30,9 @@ export class SkinPredictComponent implements OnInit {
   public loading = false;
   public formControl = new FormControl('', Validators.required);
 
+  public isCameraMode:any
+  public webcamImage: WebcamImage;
+  public capture$ = new Subject<null>()
   constructor(
     private service: HAM10000Service,
     private spinner: NgxSpinnerService,
@@ -45,6 +50,25 @@ export class SkinPredictComponent implements OnInit {
       `./assets/model-tf/${selectedValue}`
     );
     this.spinner.hide();
+  }
+  public capture(){
+    if(this.webcamImage ===null){
+
+      this.capture$.next()
+    }
+    else{
+      this.webcamImage = null
+    }   
+  }
+  public setCameraMode(s:boolean){
+    this.isCameraMode = s;
+    this.image = null;
+    this.webcamImage = null
+  }
+  public handleImage(webcamImage: WebcamImage): void {
+    console.log(webcamImage);
+    this.webcamImage = webcamImage
+    this.image = webcamImage;
   }
   public onFileChanged(event) {
     const file = event.target.files[0];
